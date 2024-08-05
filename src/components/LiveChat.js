86cf54f1074c2dd00dfc423747c7922850ca7bcb
Aashish Yadav <yadavaashish276@gmail.com>
2024-08-05 22:54:33 +0530

    Name changed for checking purpose

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LiveChat() {
   return (
-    <div className="w-full h-[600] ml-2 p-2 border-black">LiveChat</div>
+    <div className="w-full h-[600] ml-2 p-2 border-black">Live Chat</div>
   )
 }
 
